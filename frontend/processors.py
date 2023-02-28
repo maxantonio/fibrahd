e066137dd9d00f92b8fa5db404c932c86fe2eaed
Tania <tbarrosolopez@gmail.com>
2023-02-28 13:44:10 -0500

    añadiendo contenido a sustentabilidad

--- frontend/processors.py
+++ frontend/processors.py
@@ -3,6 +3,6 @@ from django.conf import settings
 
 def docs_base_url(request):
     return {
-        'css_js_version': '?v=1.0.4',
+        'css_js_version': '?v=1.0.5',
         'docs_base_url': settings.DOCS_BASE_URL
     }
