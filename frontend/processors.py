8765d0eab5e38de7141674db5882de3291b1f725
Tania <tbarrosolopez@gmail.com>
2023-03-03 18:33:41 -0500

    cambios generales

--- frontend/processors.py
+++ frontend/processors.py
@@ -3,6 +3,6 @@ from django.conf import settings
 
 def docs_base_url(request):
     return {
-        'css_js_version': '?v=1.0.5',
+        'css_js_version': '?v=1.0.6',
         'docs_base_url': settings.DOCS_BASE_URL
     }
